fad2def0f0acd7f667d6a246c564f7e692fa3ee0
PC-SCHULE\joele <joel.eyer@bwz-rappi.ch>
2025-02-21 12:01:09 +0100

    add action listeners

--- src/main/java/view/DetailDialogue.java
+++ src/main/java/view/DetailDialogue.java
@@ -3,6 +3,8 @@ package view;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DetailDialogue extends JDialog {
 
@@ -17,7 +19,7 @@ public class DetailDialogue extends JDialog {
     JTextField txtLastName = new JTextField();
     JLabel lblFirstName = new JLabel("Vorname:");
     JTextField txtFirstName = new JTextField();
-    JLabel lblSex = new JLabel("Sex:");
+    JLabel lblSex = new JLabel("Geschlecht:");
     JRadioButton radMale = new JRadioButton("Mann");
     JRadioButton radFemale = new JRadioButton("Frau");
     JLabel lblDateOfBirth = new JLabel("Geburtsdatum:");
@@ -33,6 +35,7 @@ public class DetailDialogue extends JDialog {
 
     JButton btnSave = new JButton("Speichern");
     JButton btnCancel = new JButton("Abbrechen");
+    JButton btnClear = new JButton("Leeren");
     // Dialogue Items end
 
     // Containers start
@@ -70,6 +73,27 @@ public class DetailDialogue extends JDialog {
         pnlData.setLayout(new GridLayout(7, 2, 5, 5));
         pnlButtons.setLayout(new FlowLayout(FlowLayout.CENTER));
 
+
+        btnSave.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnClear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                txtLastName.setText("");
+                txtFirstName.setText("");
+
+            }
+        });
+
         pnlTitle.add(lblTitle, BorderLayout.WEST);
         pnlTitle.add(sprData, BorderLayout.SOUTH);
 
@@ -99,6 +123,7 @@ public class DetailDialogue extends JDialog {
 
         pnlButtons.add(btnSave);
         pnlButtons.add(btnCancel);
+        pnlButtons.add(btnClear);
 
         // Add panels start
         pnlContent.add(pnlTitle, BorderLayout.NORTH);
